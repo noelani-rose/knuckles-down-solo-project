b7d2ab0145d90d068c86420054a6a716dc05676b
Noelani West <noelani.rose14@gmail.com>
2022-11-13 13:39:42 -0600

    create radio group for experience level and text fields for PRs on registration form

--- README.md
+++ README.md
@@ -119,3 +119,34 @@ This code is also heavily commented. We recommend reading through the comments, 
 ## Update Documentation
 
 Customize this ReadMe and the code comments in this project to read less like a starter repo and more like a project. Here is an example: https://gist.github.com/PurpleBooth/109311bb0361f32d87a2
+
+
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+<!-- 
+export default function RowRadioButtonsGroup() {
+  return (
+    <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+        <FormControlLabel
+          value="disabled"
+          disabled
+          control={<Radio />}
+          label="other"
+        />
+      </RadioGroup>
+    </FormControl>
+  );
+} -->
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 
 function RegisterForm() {
   const [username, setUsername] = useState('');
@@ -54,15 +63,37 @@ function RegisterForm() {
       </div>
 
         <div>
-          <label htmlFor = "experience">
-            
-          <input type = "radio" value = "0"></input>
-          <input type = "radio" value = "1"></input>
-          <input type = "radio" value = "2"></input>
-
-          </label>
+      <FormControl>
+      <FormLabel id="experience-level">Experience Level</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group" 
+      >
+        <FormControlLabel value="0" control={<Radio />} label="Beginner" />
+        <FormControlLabel value="1" control={<Radio />} label="Intermediate" />
+        <FormControlLabel value="2" control={<Radio />} label="Advanced" />
+      </RadioGroup>
+    </FormControl>
         </div>
+      <div>
+      <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 0.5, width: '10ch', height: '10ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Snatch" variant="standard" />
+      <TextField id="outlined-basic" label="Clean and Jerk" variant="standard" />
+      <TextField id="outlined-basic" label="Back Squat" variant="standard" />
+      <TextField id="outlined-basic" label="Front Squat" variant="standard" />
 
+
+    </Box>
+
+      </div>
 
       <div>
         <input className="btn" type="submit" name="submit" value="Register" />
