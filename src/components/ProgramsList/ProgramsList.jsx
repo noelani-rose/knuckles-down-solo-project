99b17c59892bca3f71f0891f63f8a8f026d262f3
Noelani West <noelani.rose14@gmail.com>
2022-11-14 14:38:53 -0600

    create 'choose program' button on home page, work on routing to /programs

---
+++ src/components/ProgramsList/ProgramsList.jsx
@@ -0,0 +1,15 @@
+import {HashRouter as Router, Route} from 'react-router-dom';
+
+
+
+function ProgramsList () {
+    return (
+        <div>
+        <Route exact path = '/programs'>
+            Program List...
+        </Route>
+        </div>
+    )
+}
+
+export default ProgramsList; 
