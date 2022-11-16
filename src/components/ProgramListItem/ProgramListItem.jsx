be4fddd5a046fa00eb28527256fa39d47966c495
Noelani West <noelani.rose14@gmail.com>
2022-11-16 10:04:27 -0600

    work on mui confirmation dialogue

--- src/components/ProgramListItem/ProgramListItem.jsx
+++ src/components/ProgramListItem/ProgramListItem.jsx
@@ -1,3 +1,17 @@
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+
+
+
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 
@@ -5,7 +19,23 @@ import { useDispatch, useSelector } from 'react-redux'
 
 function ProgramListItem () {
     const programs = useSelector(store => store.program)
-    
+    console.log('programs in the store right now are', programs)
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        dispatch({
+            type: "FETCH_USER_PROGRAM"
+        })
+    })
+
+    const addUserProgram = (id) => {
+        console.log('in adduserprogram function sending over program id')
+        dispatch({
+            type: 'ADD_USER_PROGRAM',
+            payload: id
+        })
+    }
 
 
 
@@ -29,10 +59,11 @@ function ProgramListItem () {
                         <td>{program.intensity}</td>
                         <td>{program.volume}</td>
                         <td>{program.good_for}</td>
+                        <td><button onClick ={addUserProgram}>Select</button></td>
                     </tr>
                 ))}
             </tbody>
-        </table>                
+        </table>
         </>
     )
 }
