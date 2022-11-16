5ff0c0eb17a5d75f6f08d40cf7f5415298952f0c
Noelani West <noelani.rose14@gmail.com>
2022-11-15 18:15:54 -0600

    create dispatch on ProgramList to FETCH_PROGRAMS

--- server/routes/program.router.js
+++ server/routes/program.router.js
@@ -7,6 +7,17 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   // GET route code here
+  const queryText = `SELECT * FROM "programs ORDER BY "id" ASC`;
+
+  pool.query(queryText)
+  .then(dbResult => {
+    res.send(dbResult.rows)
+    console.log('what is the result from programs db?', dbResult.rows)
+  })
+  .catch(error => {
+    console.log('error getting all programs', error)
+    res.sendStatus(500)
+  })
 });
 
 /**
--- src/components/ProgramsList/ProgramsList.jsx
+++ src/components/ProgramsList/ProgramsList.jsx
@@ -1,7 +1,18 @@
 import ProgramListItem from "../ProgramListItem/ProgramListItem";
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
 
 function ProgramsList () {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({
+            type: 'FETCH_PROGRAMS'
+        })
+    }, [])
+
+
+
     return (
          <div>
              Program List...
